2ec091facc8c56b17fdf9cd45b7dc8f482f91e8b
Ka Hei <ninankh@umich.edu>
2026-08-11 14:29:54 -0400

    updated brushed with two minor fixes

--- opensourceleg/actuators/brushed.py
+++ opensourceleg/actuators/brushed.py
@@ -466,10 +466,10 @@ class MaxonActuator(PositionControlActuatorBase):
         self.d_term_filtered_last = d_term_filtered
 
         # only integrate when not saturated (prevent windup)
-        if -(self.pwm_maximum_command - 5) < self.last_pwm < (self.pwm_maximum_command - 5):
+        if -(self.pwm_maximum_command - 0.05) < self.last_pwm < (self.pwm_maximum_command - 0.05):
             self.i_term = self.i_term + (self.k_i * error_encoder * dt)
 
-        pwm_feedback = int(p_term + self.i_term + d_term_filtered) / 100
+        pwm_feedback = p_term + self.i_term + d_term_filtered / 100
         self.last_pwm = pwm_feedback
 
         return pwm_feedback
